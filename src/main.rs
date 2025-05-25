4ceb4de355adede6d22dc1630eeb7bb5eaa20e7b
Syed <syed@designerparfums.com>
2025-05-25 20:42:06 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use dioxus::{prelude::*, Ok};
+use dioxus::{prelude::*};
 
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 
@@ -19,12 +19,13 @@ pub fn Main() -> Element {
 
     let mut toggle_form = use_signal(|| false);
     let mut button_text = use_signal(|| String::new());
+    let mut response_msg = use_signal(|| String::new());
 
     let mut first_name = use_signal(|| String::new());
     let mut last_name = use_signal(|| String::new());
     let mut email = use_signal(|| String::new());
     let mut password = use_signal(|| String::new());
-
+    //...
     if toggle_form() == false {
         button_text.set(String::from("Create Account"));
     } else {
@@ -63,6 +64,11 @@ pub fn Main() -> Element {
                         }
                     }
                 }
+            }
+
+            div { 
+                class : "msg",
+                "{response_msg}" 
             }
 
             if toggle_form() == false {
@@ -115,9 +121,14 @@ pub fn Main() -> Element {
                     onclick : move |_| async move {
                         if toggle_form() == false {
                             // Register user
-                            register(first_name(), last_name(), email(), password()).await.unwrap();
+                            let response_text = register(first_name(), last_name(), email(), password()).await.unwrap();
+
+                            response_msg.set(response_text);
                         } else {
                             // Signin user
+                            let response_text = login(email(), password()).await.unwrap();
+
+                            response_msg.set(response_text);
                         }
                     },
                     {button_text()}
@@ -128,9 +139,9 @@ pub fn Main() -> Element {
                 div {
                     class: "row",
                     button {
-                        class : "btn-signin-github",
-                        onclick : move |_| {
-                            // Signin with google
+                        class : "btn-signin-google",
+                        onclick : move |_| async move {
+                            // Sign in with Google
                         },
                         img {
                             src: asset!("/assets/google-icon.svg")
@@ -143,73 +154,120 @@ pub fn Main() -> Element {
     }
 }
 
-/// Echo the user input on the server.
 #[server()]
-async fn register(first_name: String, last_name : String, email : String, password : String) -> Result<(), ServerFnError> {
+async fn register(
+    first_name : String, 
+    last_name : String, 
+    email : String, 
+    password : String) -> Result<String, ServerFnError> {
 
-    // use rusqlite::Connection;
+    use rusqlite::Connection;
+    let con: Connection = Connection::open("./users.db").unwrap();
 
-    // let con: Connection = Connection::open("./users.db").unwrap();
+    con.execute(
+        "CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            password TEXT NOT NULL,
+            provider TEXT NOT NULL
+        )",
+        [],
+    ).unwrap();
 
-    // con.execute(
-    //     "CREATE TABLE IF NOT EXISTS users (
-    //         id INTEGER PRIMARY KEY,
-    //         first_name TEXT NOT NULL,
-    //         last_name TEXT NOT NULL,
-    //         email TEXT NOT NULL,
-    //         password TEXT NOT NULL,
-    //         provider TEXT NOT NULL
-    //     )",
-    //     [],
-    // ).unwrap();
+    let mut row = con.query_row("SELECT email FROM users 
+        WHERE email = ?1 AND provider = 'classic'", &[&email], |row| {
+        row.get::<usize, String>(0)
+    });
 
-    // let mut row = con.query_row("SELECT email FROM users WHERE email = ?1 AND provider = 'classic'", &[&email], |row| {
-    //     row.get::<usize, String>(0)
-    // }).unwrap();
+    let message = match row {
+        Err(ref e) => {
+            // Create User
+            use argon2::Argon2;
+            use argon2::password_hash::{
+                PasswordHasher, 
+                SaltString,
+                rand_core::OsRng
+            };
 
-    //Err(QueryReturnedNoRows)
-    // let mesage = match row {
-    //     Err(ref e) => {
-    //         println!("No : {:?}", row);
-    //         "User created".to_string()
-    //     },
-    //     _ => { "User exists".to_string() }
-    // };
+            let argon2 = Argon2::default(); 
 
-    // println!("User : {:?}", row);
+            let salt = SaltString::generate(&mut OsRng);  
 
-    //Ok(mesage)
-    Ok(())
+            let password_hash = argon2.hash_password(password.as_bytes(), &salt)
+                .unwrap()
+                .to_string();
 
-    // use argon2::Argon2;
-    // use argon2::password_hash::{
-    //     PasswordHasher, 
-    //     PasswordVerifier,
-    //     SaltString,
-    //     rand_core::OsRng
-    // };
+            println!("{}", password_hash);
 
-    // let argon2 = Argon2::default(); 
+            let mut stmt = con.prepare("INSERT INTO users 
+                (first_name, last_name, email, password, provider) 
+                VALUES (?, ?, ?, ?, ?)").unwrap();
+            
+            stmt.execute(&[
+                &first_name, 
+                &last_name, 
+                &email, 
+                &password_hash,
+                "classic"
+            ]).unwrap();
 
-    // let salt = SaltString::generate(&mut OsRng);  
+            { "User created." }
+        },
+        Ok(_) => { 
+            "A user with this email already exists." 
+        }
+    };
 
-    // let password_hash = argon2.hash_password(password.as_bytes(), &salt)
-    //     .unwrap()
-    //     .to_string();
+    Ok(message.to_string())
+}
 
-    // println!("{}", password_hash);
+#[server()]
+async fn login(
+    email : String, 
+    password : String) -> Result<String, ServerFnError> {
 
-    // let mut stmt = con.prepare("INSERT INTO users 
-    //     (first_name, last_name, email, password, provider) 
-    //     VALUES (?, ?, ?, ?, ?)").unwrap();
-    
-    // stmt.execute(&[
-    //     &first_name, 
-    //     &last_name, 
-    //     &email, 
-    //     &password_hash,
-    //     "classic"
-    // ]).unwrap();
+    use rusqlite::Connection;
+    let con: Connection = Connection::open("./users.db").unwrap();
 
-    //Ok(())
+    let mut row = con.query_row("SELECT id, first_name, last_name, password 
+        FROM users WHERE email = ?1 AND provider = 'classic'", &[&email], |row| {
+        Ok(
+            (
+                row.get::<usize, usize>(0).unwrap(),
+                row.get::<usize, String>(1).unwrap(),
+                row.get::<usize, String>(2).unwrap(),
+                row.get::<usize, String>(3).unwrap()
+            )
+        )
+    });
+
+    let message = match row {
+        Ok(user) => {
+            
+            use argon2::Argon2;
+            use argon2::password_hash::{
+                PasswordHash, 
+                PasswordVerifier
+            };
+
+            let argon2 = Argon2::default();
+
+            let hash = PasswordHash::new(&user.3).unwrap();
+
+            let result = argon2.verify_password(password.as_bytes(), &hash);
+
+            if result.is_ok(){
+                "Login successful"
+            }else{
+                "Invalid login details"
+            }
+        },
+        Err(ref e) => {
+            "Invalid login details"
+        }, 
+    };
+
+    Ok(message.to_string())
 }
